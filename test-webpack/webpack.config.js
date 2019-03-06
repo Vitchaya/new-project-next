0aa073f799562e8cc2c07c666010a4e2b294bfb0
wait <noppakorn@muze.co.th>
2019-03-06 17:22:45 +0700

    add webpack example

---
+++ test-webpack/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+}
---
+++ webpack/index.js
@@ -0,0 +1,7 @@
+const log = require('./log')
+const css = require('./style.css')
+
+let a = 1
+log(`Webpack ${a}`)
+a = 2
+log(`Error ${a}`)
